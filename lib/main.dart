34163f4693e125a14539704d8ba8ed9e959ae4d4
tnantoka <tnantoka@bornneet.com>
2019-04-06 23:00:25 +0900

    Basic example

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:box2d_flame/box2d.dart';
+import 'package:spritewidget/spritewidget.dart';
+import 'package:flutter_shapes/flutter_shapes.dart';
 
 void main() => runApp(MyApp());
 
@@ -25,35 +28,126 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Scene scene;
+
+  @override
+  void initState() {
+    super.initState();
+
+    scene = Scene(const Size(1024.0, 1024.0));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Container(
-          child: CustomPaint(painter: _MyPainter()),
-        ),
-      ),
+      body: SpriteWidget(scene),
     );
   }
 }
 
-class _MyPainter extends CustomPainter {
+class Scene extends NodeWithSize {
+  Scene(Size size) : super(size) {
+    userInteractionEnabled = true;
+    world = World.withGravity(Vector2(0.0, 9.8));
+    _addCircle(Offset(512, 512));
+    _addFloor();
+  }
+
+  World world;
+
   @override
-  bool shouldRepaint(_MyPainter oldDelegate) {
-    return false;
+  void update(double dt) {
+    super.update(dt);
+    world.stepDt(dt, 10, 10);
   }
 
   @override
-  void paint(Canvas canvas, Size size) {
+  handleEvent(SpriteBoxEvent event) {
+    if (event.type == PointerDownEvent) {
+      Offset localPosition = convertPointToNodeSpace(event.boxPosition);
+      _addCircle(localPosition);
+    }
+    return true;
+  }
+
+  void _addCircle(Offset position) {
+    final PhysicsNode node = PhysicsNode()
+      ..type = ShapeTypes.Circle
+      ..radius = 30
+      ..position = position;
+    addChild(node);
+
+    final CircleShape shape = CircleShape();
+    shape.radius = 30;
+
+    final FixtureDef fixtureDef = FixtureDef();
+    fixtureDef.friction = 0.5;
+    fixtureDef.restitution = 0.4;
+    fixtureDef.density = 1;
+    fixtureDef.shape = shape;
+
+    final BodyDef bodyDef = BodyDef();
+    bodyDef.linearVelocity = Vector2(0.0, 50.0);
+    bodyDef.position = Vector2(node.position.dx, node.position.dy);
+    bodyDef.type = BodyType.DYNAMIC;
+    bodyDef.bullet = true;
+
+    final Body body = world.createBody(bodyDef);
+    body.createFixtureFromFixtureDef(fixtureDef);
+
+    node.body = body;
+  }
+
+  void _addFloor() {
+    final PhysicsNode node = PhysicsNode()
+      ..type = ShapeTypes.Rect
+      ..radius = 1024
+      ..position = const Offset(512, 2000);
+    addChild(node);
+
+    final PolygonShape shape = PolygonShape();
+    shape.setAsBoxXY(1024, 1024);
+
+    final FixtureDef fixtureDef = FixtureDef();
+    fixtureDef.friction = 0.5;
+    fixtureDef.restitution = 0.4;
+    fixtureDef.density = 1;
+    fixtureDef.shape = shape;
+
+    final BodyDef bodyDef = BodyDef();
+    bodyDef.position = Vector2(node.position.dx, node.position.dy);
+    bodyDef.type = BodyType.STATIC;
+    bodyDef.bullet = true;
+
+    final Body body = world.createBody(bodyDef);
+    body.createFixtureFromFixtureDef(fixtureDef);
+
+    node.body = body;
+  }
+}
+
+class PhysicsNode extends Node {
+  Body body;
+  ShapeTypes type;
+  double radius;
+
+  @override
+  void update(double dt) {
+    super.update(dt);
+    position = Offset(body.position.x, body.position.y);
+  }
+
+  @override
+  void paint(Canvas canvas) {
     final Paint paint = Paint()
       ..color = Colors.grey
       ..style = PaintingStyle.fill
       ..strokeWidth = 3;
-    const double radius = 30;
-    final Offset c = Offset(size.width * 0.5, size.height * 0.5);
-    canvas.drawCircle(c, radius, paint);
+    const Offset center = Offset.zero;
+    Shapes(canvas: canvas, paint: paint, radius: radius, center: center)
+        .drawType(type);
   }
 }
